858c7728057619ee2648c08159ee0634538568d4
仕永 董 <chps986213@gmail.com>
2020-06-18 00:27:00 +0800

    new

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__) # __name__ 為 python 內建的變數，他會儲存目前
 
 def home():
 
-    return render_template("index.html", title="五官大於三觀")
+    return render_template("index.html", title="逢甲美食地圖")
 
 
 
@@ -17,79 +17,79 @@ def home():
 
 def IT200():
 
-    return render_template("IT200.html", title="五官大於三觀")
+    return render_template("IT200.html", title="逢甲美食地圖")
 
 @app.route("/It/300") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def IT300():
 
-    return render_template("IT300.html", title="五官大於三觀")
+    return render_template("IT300.html", title="逢甲美食地圖")
 
 @app.route("/JPN/100") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def JPn100():
 
-   return render_template("JPn100.html", title="五官大於三觀")
+   return render_template("JPn100.html", title="逢甲美食地圖")
 
 @app.route("/JPN/200") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def JPn200():
 
-   return render_template("JPn200.html", title="五官大於三觀")
+   return render_template("JPn200.html", title="逢甲美食地圖")
 
 @app.route("/JPN/300") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def JPn300():
 
-   return render_template("JPn300.html", title="五官大於三觀")
+   return render_template("JPn300.html", title="逢甲美食地圖")
 
 @app.route("/BD/100") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def BD100():
 
-   return render_template("BD100.html", title="五官大於三觀")
+   return render_template("BD100.html", title="逢甲美食地圖")
 
 @app.route("/CUR/200") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def CUR200():
 
-   return render_template("CUR200.html", title="五官大於三觀")
+   return render_template("CUR200.html", title="逢甲美食地圖")
 
 @app.route("/CUR/300") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def CUR300():
 
-   return render_template("CUR300.html", title="五官大於三觀")
+   return render_template("CUR300.html", title="逢甲美食地圖")
 
 @app.route("/KOR/200") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def KOR200():
 
-   return render_template("KOR200.html", title="五官大於三觀")
+   return render_template("KOR200.html", title="逢甲美食地圖")
 
 @app.route("/KOR/300") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def KOR300():
 
-   return render_template("KOR300.html", title="五官大於三觀")
+   return render_template("KOR300.html", title="逢甲美食地圖")
 
 @app.route("/ND/100") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def ND100():
 
-   return render_template("ND100.html", title="五官大於三觀")
+   return render_template("ND100.html", title="逢甲美食地圖")
 
 @app.route("/SA/200") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def SA200():
 
-   return render_template("SA200.html", title="五官大於三觀")
+   return render_template("SA200.html", title="逢甲美食地圖")
 
 @app.route("/TW/100") #函式的裝飾 Decorator，以底下函式為基礎，提供附加的功能，這邊 "/" 代表根目錄
 
 def TW100():
 
-   return render_template("TW100.html", title="五官大於三觀")
+   return render_template("TW100.html", title="逢甲美食地圖")
 
 if __name__ == "__main__": #如果以主程式運行
 
